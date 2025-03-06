9d9cc30e62802872b787167829f9f290bf450e23
yassine <yassinesebaihia1@gmail.com>
2025-03-06 15:06:01 +0100

    Test request

--- src/Controller/ApiAccountController.php
+++ src/Controller/ApiAccountController.php
@@ -7,11 +7,22 @@ use App\Repository\AccountRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\DocBlock\Serializer;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Encoder\EncoderInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 class ApiAccountController extends AbstractController
 {
     public function __construct(
-        private AccountRepository $accountRepository
+        private AccountRepository $accountRepository,
+        private readonly EntityManagerInterface $em,
+        private readonly SerializerInterface $serializer
     ) {}
 
     #[Route('/api/accounts', name: 'api_account_all')]
@@ -23,5 +34,30 @@ class ApiAccountController extends AbstractController
             [],
             ['groups' => 'account:read']
         );
+    }
+
+    public function addAccount(Request $request): Response
+    {
+        //Récupération le body de la requête
+        $request = $request->getContent();
+        //Convertir en objet account
+        $account = $this->serializer->deserialize($request, Account::class, 'json');
+        //Tester si le compte n'existe pas
+        if (!$this->accountRepository->findOneBy([
+            "email" => $account->getEmail()
+            ])) {
+            $this->em->persist($account);
+            $this->em->flush();
+            $code = 201;
+        }
+        //Si le compte existe deja via l'adresse email
+        else {
+            $account = "L'utilisateur existe déjà";
+            $code = 400;
+        }
+        return $this->json($account, $code, [
+            "Access-Control-Allow-Origin" => "*",
+            "Content-Type" => "application/json"
+        ], []);
     }
 }
