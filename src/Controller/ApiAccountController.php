39c4b44524f8b07ef12b865b98e17d0f662d0177
yassine <yassinesebaihia1@gmail.com>
2025-03-07 10:58:38 +0100

    add api update

--- src/Controller/ApiAccountController.php
+++ src/Controller/ApiAccountController.php
@@ -46,6 +46,7 @@ class ApiAccountController extends AbstractController
         if (!$this->accountRepository->findOneBy([
             "email" => $account->getEmail()
             ])) {
+                if(!$this->accountRepository->fin)
             $this->em->persist($account);
             $this->em->flush();
             $code = 201;
@@ -60,4 +61,85 @@ class ApiAccountController extends AbstractController
             "Content-Type" => "application/json"
         ], []);
     }
+
+    public function updateAccount(Request $request): Response
+{
+    // Récupération du contenu de la requête
+    $requestContent = $request->getContent();
+
+    //Convertir en objet account
+    $account = $this->serializer->deserialize($requestContent, Account::class, 'json');
+
+    // check si le compte existe via l'email
+    $accountInBDD = $this->accountRepository->findOneBy([
+        "email" => $account->getEmail()
+    ]);
+
+    if (!$accountInBDD) {
+        return $this->json(["error" => "L'email n'existe pas en bdd"], 
+    $code= 404);
+    }
+
+    // Mise à jour des données
+    $accountInBDD->setFirstname($account->getFirstname());
+    $accountInBDD->setLastname($account->getLastname());
+    $code= 200;
+    // Sauvegarde en base de données
+    $this->em->persist($accountInBDD);
+    $this->em->flush();
+
+    return $this->json($account, $code, [
+        "Access-Control-Allow-Origin" => "*",
+        "Content-Type" => "application/json"
+    ], []);
+
+}
+// public function deleteAccount(Request $request): Response
+// {
+//     // Récupération du contenu de la requête
+//     $requestContent = $request->getContent();
+
+//     // Convertir en objet Account
+//     $account = $this->serializer->deserialize($requestContent, Account::class, 'json');
+
+//     // Vérification de l'existence du compte via l'email
+//     $accountInBDD = $this->accountRepository->findOneBy([
+//         "email" => $account->getEmail()
+        
+//     ]);
+
+//     if (!$accountInBDD) {
+//         $code = 404;
+//         return $this->json(["error" => "L'email n'existe pas en BDD"], 
+//     $code);
+//     }
+
+//     // Suppression du compte
+//     $this->em->remove($accountInBDD);
+//     $this->em->flush();
+//     $code = 200;
+//     return $this->json($account, $code, [
+//         "Access-Control-Allow-Origin" => "*",
+//         "Content-Type" => "application/json"
+//     ], []);
+// }
+public function deleteAccount(int $id): Response
+{
+    // récupération de l'id
+    $account = $this->accountRepository->find($id);
+    // verifer si le compte existe
+    if (!$account) {
+        return $this->json(["error" => "l'id n'existe pas"], $code =404);
+    }
+
+    // Suppression du compte
+    $this->em->remove($account);
+    $this->em->flush();
+
+    return $this->json($account, $code = 200, [
+        "Access-Control-Allow-Origin" => "*",
+        "Content-Type" => "application/json"
+    ], []);
+}
+
 }
